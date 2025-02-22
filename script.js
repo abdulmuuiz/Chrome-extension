26c19818ba557948d3ae14502f4c79c4c64a1393
Abdul Muuiz <aanmuiz@gmail.com>
2025-02-22 12:44:16 +0500

    This is the final

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ replaceText(document.body)
 
 function replaceText(element) {
     if(element.hasChildNodes()) {
-        element.childNodes.array.forEach(element => {
-            replaceText
-        });
+        element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        element.textContent = element.textContent.replace(/coronavirus/gi, "⬛")
+        if (element.textContent.match(/coronavirus/gi)) {
+            // console.log(element.parentElement)
+            // element.parentElement.remove();
+            const newElement = document.createElement("span");
+            newElement.innerHTML = element.textContent.replace(/coronavirus/gi, '<span class="rainbow"> CoronaVirus </span>');
+            element.replaceWith(newElement)
+        }
+        // element.textContent = element.textContent.replace(/coronavirus/gi, "⬛")
 
     }
+
 }
