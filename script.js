4f8e8fde9d64e28f511b4cc6c52d634a472fa616
Abdul Muuiz <aanmuiz@gmail.com>
2025-02-22 11:54:33 +0500

    another commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("hello good morning beautiful!")
+replaceText(document.body)
+
+
+function replaceText(element) {
+    if(element.hasChildNodes()) {
+        element.childNodes.array.forEach(element => {
+            replaceText
+        });
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/coronavirus/gi, "⬛")
+
+    }
+}
